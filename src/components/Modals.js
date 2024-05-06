bf07150c1cb7a4c54b4277827943737883208b57
MuneebWaliKhan09 <muneebwali809@gmail.com>
2024-05-06 20:15:15 +0500

    push

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button, Modal, TouchableOpacity} from 'react-native';
+import Modal from 'react-native-modal';
+import React, {useState} from 'react';
 
 const Modals = () => {
-  return (
-    <View>
-      <Text>Modals</Text>
-    </View>
-  )
-}
+  const [OpenModal, setOpenModal] = useState(false);
 
-export default Modals
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button
+        title="Open Modal"
+        onPress={() => {
+          setOpenModal(true);
+        }}
+      />
+
+      {/* custom modal */}
+      {/* <Modal onRequestClose={()=> setOpenModal(false)} visible={OpenModal} transparent style={{flex: 1}}>
+        <TouchableOpacity
+          onPress={() => setOpenModal(false)}
+          activeOpacity={1}
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,.2)',
+            width: '100%',
+            height: '100%',
+          }}>
+          <View
+            style={{
+              width: '90%',
+              height: 200,
+              backgroundColor: 'white',
+            }}>
+                <Text style={{textAlign:"center", fontSize:20, marginTop:35}}>Do You want to Log Out ? </Text>
+               <View style={{width:"40%",height:"100%", alignSelf:"center", marginTop:40}}>
+               <Button  title='Log Out'/>
+               </View>
+            </View>
+        </TouchableOpacity>
+      </Modal> */}
+
+      {/* react native modal */}
+      <Modal visible>
+
+      </Modal>
+      
+    </View>
+  );
+};
+
+export default Modals;
+
+
+// Modal
+
+// 1) onRequestClose || can  close the modal by back button in mobile phone
